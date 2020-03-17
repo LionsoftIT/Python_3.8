e1c1c470a78c0029d4c2c968bebb176d51ca6081
LionsoftIT <55684584+LionsoftIT@users.noreply.github.com>
2020-03-17 15:00:11 +0100

    Controllo voti (sufficiente / insufficiente)

---
+++ controllo_voti.py
@@ -0,0 +1,18 @@
+"""
+Creatore: LEONARDO ESSAM DEI ROSSI
+E-Mail: leonardo.deirossi@icloud.com
+"""
+
+print("Benvenuti nel programma di controllo voti")
+
+voto = 0.0
+
+voto = float(input("Inserisci il tuo voto: "))
+#print(voto) - Istruzione di debug
+
+if voto<=5.0:
+    print("Il tuo voto è insufficiente!")
+else:
+    print("Il tuo voto è sufficiente!")
+print("Grazie e arrivederci")
+
